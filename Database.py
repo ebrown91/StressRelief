0c57a217d133899ce5d6e47cb013a2b148673847
ebrown91 <ebrown@benoit-inc.com>
2021-08-24 14:26:46 -0500

    added database functionality

--- Database.py
+++ Database.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 conn = sqlite3.connect('my_data.db')
 c = conn.cursor()
-
+class Database:
 
 def create_table():
     c.execute('''CREATE TABLE IF NOT EXISTS stressrelief (id REAL, Size INTEGER, weight INTEGER,Connection TEXT,wee TEXT,temperature INTEGER, time INTEGER, material TEXT)
@@ -18,6 +18,38 @@ def csv_reader(file_name):
     for row in c.fetchall():
         print(row)
 
+def add_one(size,weight,connection,heatnumber,material, temperature,time):
+    conn=sqlite3.connect('my_data.db')
+    c=conn.cursor()
+    c.execute('SELECT * FROM stressrelief2')
+    length=len(c.fetchall())+1
+    c.execute("INSERT INTO stressrelief2 VALUES(?,?,?,?,?,?,?,?)",(length,size,weight,connection,heatnumber,material,temperature,time))
+    conn.commit()
+    conn.close()
+
+#Look up OD and return items with OD
+def size_lookup(a):
+    c.execute("SELECT * FROM stressrelief2 WHERE Size=(?)",(a,))
+    #c.execute("SELECT * FROM stressrelief WHERE Size=(?) AND weight=(?)",(a,b))
+    items = c.fetchall()
+    if len(items)==0:
+        print('There is no OD in that size')
+        b=input('Weight: ')
+        h=input('Connection: ')
+        d= input('Heat Number: ')
+        e = input('Material: ')
+        f =input('Temperature F: ')
+        g= input('Time in seconds: ')
+        add_one(a,b,h,d,e,f,g)
+        print('New data inputed sucessfully')
+    else:
+        for item in items:
+            print (item)
+
+def display_db():
+    c.execute("SELECT * FROM stressrelief2")
+    for row in c.fetchall():
+        print(row)
 
 def graph_data():
 
