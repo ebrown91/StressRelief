5e4642e31ed57323381d87fda7ed96c82d72ddf1
ebrown91 <ebrown@benoit-inc.com>
2021-08-25 08:51:48 -0500

    organizing database class

--- Database.py
+++ Database.py
@@ -5,27 +5,35 @@ conn = sqlite3.connect('my_data.db')
 c = conn.cursor()
 class Database:
 
-def create_table():
-    c.execute('''CREATE TABLE IF NOT EXISTS stressrelief (id REAL, Size INTEGER, weight INTEGER,Connection TEXT,wee TEXT,temperature INTEGER, time INTEGER, material TEXT)
-     ''')
+    def create_table():
+        c.execute('''CREATE TABLE IF NOT EXISTS stressrelief 
+        (id REAL, 
+        Size INTEGER, 
+        weight INTEGER,
+        Connection TEXT,
+        wee TEXT,
+        temperature INTEGER, 
+        time INTEGER, 
+        material TEXT)
+         ''')
 
 
-def csv_reader(file_name):
-    srdata = pd.read_csv(file_name)
-    srdata.to_sql('stressrelief', conn, if_exists='replace')
-    c.execute('SELECT * FROM stressrelief')
+    def csv_reader(file_name):
+        srdata = pd.read_csv(file_name)
+        srdata.to_sql('stressrelief', conn, if_exists='replace')
+        c.execute('SELECT * FROM stressrelief')
 
-    for row in c.fetchall():
-        print(row)
+        for row in c.fetchall():
+            print(row)
 
-def add_one(size,weight,connection,heatnumber,material, temperature,time):
-    conn=sqlite3.connect('my_data.db')
-    c=conn.cursor()
-    c.execute('SELECT * FROM stressrelief2')
-    length=len(c.fetchall())+1
-    c.execute("INSERT INTO stressrelief2 VALUES(?,?,?,?,?,?,?,?)",(length,size,weight,connection,heatnumber,material,temperature,time))
-    conn.commit()
-    conn.close()
+    def add_one(size,weight,connection,heatnumber,material, temperature,time):
+        conn=sqlite3.connect('my_data.db')
+        c=conn.cursor()
+        c.execute('SELECT * FROM stressrelief2')
+        length=len(c.fetchall())+1
+        c.execute("INSERT INTO stressrelief2 VALUES(?,?,?,?,?,?,?,?)",(length,size,weight,connection,heatnumber,material,temperature,time))
+        conn.commit()
+        conn.close()
 
 #Look up OD and return items with OD
 def size_lookup(a):
